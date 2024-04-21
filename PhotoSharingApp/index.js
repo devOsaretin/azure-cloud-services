974412ff0156c81020550cfcc7c2993ba6d384ad
Osaretin Igbinobaro <mail@osaretin.dev>
2024-04-21 20:11:23 +0100

    feat: add upload file to blob storage

--- PhotoSharingApp/index.js
+++ PhotoSharingApp/index.js
@@ -1,28 +1,43 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const {BlobServiceClient} = require("@azure/storage-blob")
+const { BlobServiceClient } = require("@azure/storage-blob");
 
-const storageAccountConnectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
-const blobServiceClient = BlobServiceClient.fromConnectionString(storageAccountConnectionString);
+const storageAccountConnectionString =
+	process.env.AZURE_STORAGE_CONNECTION_STRING;
+const blobServiceClient = BlobServiceClient.fromConnectionString(
+	storageAccountConnectionString
+);
 
 //the client won't attempt to connect to Azure until an operation is invoked that requires it.
 //The client object is a lightweight object enabling access to the Azure blob storage;
 // it doesn't validate the connection or the access key beingf used.
 
-
-
 async function main() {
-    const containerName = "photos";
-    const containerClient = blobServiceClient.getContainerClient(containerName);
-    const containerExists = await containerClient.exists();
+	const containerName = "photos";
+	const containerClient = blobServiceClient.getContainerClient(containerName);
+	const containerExists = await containerClient.exists();
 
-    if (!containerExists) {
-        const createContainerResponse = await containerClient.createIfNotExists();
-        console.log(`Container ${containerName} created successfully`);
-    }else {
-        console.log(`Container ${containerName} already exists`);
-    }
+	if (!containerExists) {
+		const createContainerResponse = await containerClient.createIfNotExists();
+		console.log(`Container ${containerName} created successfully`);
+	} else {
+		console.log(`Container ${containerName} already exists`);
+	}
+
+	const filename = "docs-and-friends-selfie-stick.png";
+	const blockBlobClient = containerClient.getBlockBlobClient(filename);
+	blockBlobClient.uploadFile(filename);
+
+	const blobs = containerClient.listBlobsFlat();
+
+	let blob = await blobs.next();
+
+	while (!blob.done) {
+		console.log(
+			`${blob.value.name} --> Created: ${blob.value.properties.createdOn}   Size: ${blob.value.properties.contentLength}`
+		);
+		blob = await blobs.next();
+	}
 }
 
-
-main()
+main();
